a5066f895eb1d81c705886162db3fee13b7aea8c
Sebastian U <sesi.ulrich@outlook.de>
2021-05-26 02:02:14 +0200

    FIX: ready event fetch user #2

--- src/discord-events/ready.event.ts
+++ src/discord-events/ready.event.ts
@@ -7,10 +7,10 @@ import { Config } from "../models/config.model";
 export function onReady(client: Client): void {
     const config: Config = JSON.parse(readFileSync('./config.json', "utf-8").toString());
     if(!client.user) return;
-    
+
     success(`Bot-Client online as: ${client.user.tag}`);
     
-    client.user?.setActivity({
+    client.user.setActivity({
         type: config.activity.type,
         name: `on ${client.guilds.cache.size} Guilds`
     });
