b0bcfbdb1e7c80f018d6076c292ab157f9ac6836
Sebastian U <sesi.ulrich@outlook.de>
2021-07-21 01:15:33 +0200

    removed anybot from invite cause website doesnt exist rn

--- src/commands/invite.command.ts
+++ src/commands/invite.command.ts
@@ -15,7 +15,7 @@ export async function main(msg: Message, args: string[]): Promise<void> {
         .setColor('#22FF22')
         .setDescription(`When **you** want to invite this **beautiful** Discord-Bot into **your** Server use this [__**Link**__](${await client.generateInvite({permissions: 8})}) to invite me!`)
         .addField("\u200b", `Allready **${client.guilds.cache.size}** guilds are using my chat and love it`)
-        .addField("Anybot", `If your looking for a good administration bot too have a look at [__**Anybot**__](https://feverest.de/anybot/home)`)
+        // .addField("Anybot", `If your looking for a good administration bot too have a look at [__**Anybot**__](https://feverest.de/anybot/home)`)
         .setThumbnail(client.user?.displayAvatarURL({dynamic: true, size: 2048}) ||"")
     );
 }
