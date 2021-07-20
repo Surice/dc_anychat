ae3f558fa3c1791a3655ad2809f909d7f0601597
Sebastian U <sesi.ulrich@outlook.de>
2021-07-20 09:35:20 +0200

    messageUpdate.evnet if fixed

--- src/discord-events/messageUpdate.event.ts
+++ src/discord-events/messageUpdate.event.ts
@@ -1,10 +1,11 @@
 import { Message, PartialMessage } from "discord.js"
 import { readFileSync } from "fs";
+import { Channels } from "../__shared/models/channels.model";
 import { updateGlobalMessage } from "../__shared/service/globalMessage.service";
 
 export async function messageUpdate(oldMsg: Message | PartialMessage, msg: Message | PartialMessage): Promise<void> {
     if(msg.author?.bot) return;
-    let channels: string[] = JSON.parse(readFileSync(`${__dirname}/../__shared/data/channels.json`, "utf-8").toString());
+    let channels: Channels = JSON.parse(readFileSync(`${__dirname}/../__shared/data/channels.json`, "utf-8").toString());
 
-    if (channels.includes(msg.channel.id)) updateGlobalMessage(msg, channels);
+    if (channels[msg.guild?.id || ""] == msg.channel.id) updateGlobalMessage(msg, channels);
 }
